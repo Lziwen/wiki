486ea1962f3e680911ea930a17c235e23a8275fe
Ziwen Lin <lesleylinziwen@gmail.com>
2021-07-06 18:54:09 +0800

    3-5 integrate mybatis generator and generate scripts for demo table

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -6,4 +6,12 @@ create table `test` (
   primary key (`id`)
 ) engine=innodb default charset=utf8 comment='test';
 
+drop table if exists `demo`;
+create table `demo` (
+  `id` bigint not null comment 'id',
+  `name` varchar(50) comment 'name',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='for test';
+
+insert into `demo` (id, name) values (1, 'test');
 
