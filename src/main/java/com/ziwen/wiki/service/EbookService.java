db4bb021011c148ba92eac42dac7d5f89828a8c8
Ziwen Lin <lesleylinziwen@gmail.com>
2021-07-09 20:05:22 +0800

    3-6 develop api for ebook

---
+++ src/main/java/com/ziwen/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.ziwen.wiki.service;
+
+import com.ziwen.wiki.domain.Ebook;
+import com.ziwen.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
