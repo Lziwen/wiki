ac5607aaffdd7fe928d03c8e8ac8c2b9089c9904
Ziwen Lin <lesleylinziwen@gmail.com>
2021-07-09 20:46:15 +0800

    3-7 encapsulate request and response type

--- src/main/java/com/ziwen/wiki/controller/EbookController.java
+++ src/main/java/com/ziwen/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.ziwen.wiki.controller;
 
-import com.ziwen.wiki.domain.Ebook;
+import com.ziwen.wiki.req.EbookReq;
 import com.ziwen.wiki.resp.CommonResp;
+import com.ziwen.wiki.resp.EbookResp;
 import com.ziwen.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list1")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq ebookReq) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(ebookReq);
         resp.setContent(list);
         return resp;
     }
