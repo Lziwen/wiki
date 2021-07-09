08891b8ae677191e475903749e87ac8904c26f05
Ziwen Lin <lesleylinziwen@gmail.com>
2021-07-09 18:42:44 +0800

    3-5 add service for demo table

--- src/main/java/com/ziwen/wiki/controller/DemoController.java
+++ src/main/java/com/ziwen/wiki/controller/DemoController.java
@@ -3,17 +3,19 @@ package com.ziwen.wiki.controller;
 import com.ziwen.wiki.domain.Demo;
 import com.ziwen.wiki.service.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
     @Resource
     private DemoService demoService;
 
-    @GetMapping("/demo/list")
+    @GetMapping("/list1")
     public List<Demo> list() {
         return demoService.list();
     }
