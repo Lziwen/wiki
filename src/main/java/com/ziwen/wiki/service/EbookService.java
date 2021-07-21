cbdfb2cb1c5422ea2091b56b3d9c73890c7b6b08
Ziwen Lin <lesleylinziwen@gmail.com>
2021-07-21 17:57:11 +0800

    6-4 integrate PageHelper

--- src/main/java/com/ziwen/wiki/service/EbookService.java
+++ src/main/java/com/ziwen/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.ziwen.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ziwen.wiki.domain.Ebook;
 import com.ziwen.wiki.domain.EbookExample;
 import com.ziwen.wiki.mapper.EbookMapper;
 import com.ziwen.wiki.req.EbookReq;
 import com.ziwen.wiki.resp.EbookResp;
 import com.ziwen.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,7 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -24,7 +29,13 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total：{}", pageInfo.getTotal());
+        LOG.info("Pages：{}", pageInfo.getPages());
+
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 //            EbookResp ebookResp = new EbookResp();
