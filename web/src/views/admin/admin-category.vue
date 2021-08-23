59bee66b12efdce85729640ba6eaf2092a4d8b7e
Ziwen Lin <lesleylinziwen@gmail.com>
2021-08-22 22:17:11 -0400

    7-4 add tools to convert data into tree structure; applied to category table

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -20,7 +20,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -102,6 +102,19 @@ export default defineComponent({
       }
     ];
 
+    /**
+     * tree level1，children--> level2
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+
 
     /**
      * data query
@@ -113,7 +126,11 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           categorys.value = data.content;
+          console.log("initial data：", categorys.value);
 
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("tree structure：", level1);
         } else {
           message.error(data.message);
         }
@@ -172,7 +189,8 @@ export default defineComponent({
 
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
